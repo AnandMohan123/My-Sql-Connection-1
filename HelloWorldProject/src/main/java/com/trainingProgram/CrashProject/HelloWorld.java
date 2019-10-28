f1701b4545e680218a8eb77f20d503677a1d13f3
AnandMohan123 <anandmohan123@gmail.com>
2019-10-28 13:41:54 +0800

    Hello World Program

---
+++ HelloWorldProject/src/main/java/com/trainingProgram/CrashProject/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.trainingProgram.CrashProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+    public class HelloWorld {
+
+
+	@GetMapping
+	public String helloWorldMethod()
+	{
+		return "Hello World";
+    }
+}
